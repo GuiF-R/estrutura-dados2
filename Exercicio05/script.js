d8b85b000b8bd87f3142e2b8a530578c271fa96e
GuiF-R <guilhermerht@hotmail.com>
2023-09-14 20:16:50 -0300

    Ex5 ok

---
+++ Exercicio05/script.js
@@ -0,0 +1,11 @@
+let button = document.querySelector("button");
+button.addEventListener('click', () => {
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    let h2 = document.querySelector("h2");
+
+    if (num1 % num2 == 0)
+        h2.innerHTML = "São múltilpos";
+    else
+    h2.innerHTML = "Não são múltilpos";  
+});
